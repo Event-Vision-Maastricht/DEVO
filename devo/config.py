2a2e6d617734dcaf751fcba247d1538b24859a9e
Stanimira21 <stanimira.dimitrova04@gmail.com>
2026-06-04 02:33:21 +0200

    fix code

--- devo/config.py
+++ devo/config.py
@@ -24,7 +24,7 @@ _C.EDGE_THINNING = False
 _C.EDGE_THIN_DENSE_WINDOW = 3
 _C.EDGE_THIN_STRIDE = 2
 _C.CORR_SINGLE_LEVEL = False
-_C.FUSED_CORR_INTERP = True
+_C.FUSED_CORR_INTERP = False
 
 # Active-to-frozen edge marginalization. Frozen edges skip corr/update and keep
 # contributing their last target and confidence directly to BA.
